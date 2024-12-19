bceb44e13e20c9d1a913bb87b889908cccc520bf
Arquímides Méndez Molina <arquimides.mendez@inaoep.mx>
2024-12-19 15:39:58 -0600

    Delete kattis directory

--- kattis/ladder.py
+++
@@ -1,4 +0,0 @@
-import math
-
-h, v = map(int, input().split())
-print(math.ceil(h / math.sin(math.radians(v))))
--- kattis/modulo.py
+++
@@ -1,4 +0,0 @@
-map = {}
-for i in range(10):
-    map[int(input())%42] = 1
-print(len(map))
